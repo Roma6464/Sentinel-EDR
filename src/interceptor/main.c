821612a98268f6da18d34a926f9546410efd1efa
Romario LAURENCENA <romario.laurencena@epita.fr>
2025-12-22 17:20:34 +0000

    Fix bug lib minhook

--- src/interceptor/main.c
+++ src/interceptor/main.c
@@ -4,11 +4,13 @@
 
 
 
-int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int main()
 {
-    HMODULE hDLL = LoadLibraryW(L"Sentinel.Hook.dll");
+    // Utilise le double antislash \\ pour les chemins en C
+    HMODULE hDLL = LoadLibraryW(L"C:\\Users\\admin_edr\\Documents\\GitHub\\Sentinel-EDR\\Sentinel-EDR\\x64\\Debug\\Sentinel.Hook.dll");
     if(hDLL == NULL)
     {
+        printf("Error : %lu\n", GetLastError());
         return 1;
     }
     Sleep(500000);
